b1b37fbf11ce3338c5d8f0736e4659836d8010ef
DvitryG <dmitry135531@gmail.com>
2021-05-07 16:23:20 +0300

    добавил возможность выбора и отображения изображения

---
+++ app/src/main/java/com/chayniki/editorim/ImageEditorActivity.java
@@ -0,0 +1,14 @@
+package com.chayniki.editorim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_editor);
+    }
+}
--- app/src/main/java/com/chayniki/editorim/MainActivity.java
+++ app/src/main/java/com/chayniki/editorim/MainActivity.java
@@ -1,14 +1,72 @@
 package com.chayniki.editorim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_RESULT_CODE = 0;
+    private final static int READ_EXTERNAL_STORAGE_PERMISSION_RESULT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkReadExternalStoragePermission();
+    }
+
+    public void onClickSelectImage(View view) {
+        Intent intent  = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_RESULT_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == REQUEST_RESULT_CODE && resultCode == RESULT_OK) {
+            if (resultData != null) {
+                Uri imageUri = resultData.getData();
+                final Intent intent = new Intent(this, ImageViewerActivity.class);
+                intent.putExtra("imageUri", imageUri.toString());
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void checkReadExternalStoragePermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "Нам нужон доступ к медиа, дай!", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_PERMISSION_RESULT);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case READ_EXTERNAL_STORAGE_PERMISSION_RESULT:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Пасибки :3", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
